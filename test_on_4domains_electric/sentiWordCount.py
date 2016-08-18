cd919afe04348904814e4183f238e65efda8cd5d
LuffySir <luffysir@mail.dlut.edu.cn>
2016-08-18 09:02:16 +0800

    更新计算情感分值算法
    按照用户评分给相应的积极、消极分值赋予一定权重

--- test_on_4domains_electric/sentiWordCount.py
+++ test_on_4domains_electric/sentiWordCount.py
@@ -1,5 +1,7 @@
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.porter import PorterStemmer
+import linecache
+import math
 
 
 # pos word
@@ -19,6 +21,9 @@ path2 = 'E:\\dataset\\domain_sentiment_data\\sorted_data_acl\\electronics\\revie
 
 neg_score_path = 'E:\\dataset\\domain_sentiment_data\\sorted_data_acl\\electronics\\neg_cal_score'
 pos_score_path = 'E:\\dataset\\domain_sentiment_data\\sorted_data_acl\\electronics\\pos_cal_score'
+
+score_neg = 'E:\\dataset\\domain_sentiment_data\\sorted_data_acl\\electronics\\review_score_neg'
+score_pos = 'E:\\dataset\\domain_sentiment_data\\sorted_data_acl\\electronics\\review_score_pos'
 
 tokenizer = RegexpTokenizer(r'\w+')
 
@@ -43,69 +48,114 @@ def get_word_dict(dict_file):
 
 def match(word, sentiment_value):
     if word in mostdict:
+        sentiment_value *= 5
+    elif word in verydict:
+        sentiment_value *= 4
+    elif word in moredict:
+        sentiment_value *= 3
+    elif word in ishdict:
+        sentiment_value *= 2
+    elif word in insufficientdict:
         sentiment_value *= 1
-    if word in verydict:
-        sentiment_value *= 1
-    if word in moredict:
-        sentiment_value *= 1
-    if word in ishdict:
-        sentiment_value *= 0.5
-    if word in insufficientdict:
-        sentiment_value *= 0.25
+
+    return sentiment_value
+
+
+def match_inverse(word, sentiment_value):
     if word in inversedict:
         sentiment_value *= -1
     return sentiment_value
 
 
-def get_review_score(review):
+def cal_review_score(review):
 
-    review_score = 0
     # 评论中词的位置
     i = 0
 
+    pos_all = 0
+    neg_all = 0
     for i in range(len(review)):
         # 每次把情感词分数归零
         pos_score = 0
         if review[i] in posdict:
             pos_score += 1
             # 情感词前3个词内有表示程度的词则乘以相应的系数
-            j = i - 3
-            for j in range(i + 3):
+            j = i - 4
+            for j in range(i + 4):
                 if (j < len(review)) and (j >= 0):
                     pos_score = match(review[j], pos_score)
-                j += 1
+                if pos_score > 2:
+                    break
+
+            # 情感词前3个词内有表示程度的词则乘以相应的系数
+            j = i - 10
+            for j in range(i + 0):
+                if (j < len(review)) and (j >= 0):
+                    pos_score = match_inverse(review[j], pos_score)
 
         # 情感词分数加到评论分数中
-        review_score += pos_score
+        pos_all += pos_score
 
         neg_score = 0
         if review[i] in negdict:
             neg_score += 1
-            j = i - 3
-            for j in range(i + 3):
+            j = i - 4
+            for j in range(i + 4):
                 if (j < len(review)) and (j >= 0):
                     neg_score = match(review[j], neg_score)
-                j += 1
+                if neg_score > 2:
+                    break
 
-        review_score -= neg_score
+            j = i - 10
+            for j in range(i + 0):
+                if (j < len(review)) and (j >= 0):
+                    neg_score = match_inverse(review[j], neg_score)
 
-        i += 1
+        neg_all += neg_score
+
+    return pos_all, neg_all
+
+
+def get_review_score(review, star, k):
+
+    review_score = 0
+    pos, neg = cal_review_score(review)
+    if k <= 3:
+        print(pos, neg)
+    if float(star) == 5.0:
+        review_score = math.floor(pos * 0.9 - neg * 0.1) + 5
+    elif float(star) == 4.0:
+        review_score = math.floor(pos * 0.7 - neg * 0.3) + 3
+    elif float(star) == 2.0:
+        review_score = math.floor(pos * 0.3 - neg * 0.7) - 3
+    elif float(star) == 1.0:
+        review_score = math.floor(pos * 0.1 - neg * 0.9) - 5
 
     return review_score
 
 
-def get_score_file(rev_file, score_file):
+def get_score_file(rev_file, score_file, score_path):
     p_count = 0
     n_count = 0
     z_count = 0
+    # 评论序号
+    k = 1
     with open(rev_file, 'r') as rFile:
         with open(score_file, 'w') as wfile:
+            # with open(score_path, 'r') as sfile:
             for review in rFile:
                 review_low = review.lower()
                 review_low = review_low.strip()
+                # review_low = review_low.replace('\'t', ' not')
                 review_tokens = tokenizer.tokenize(review_low)
                 # review_str = ','.join(review_tokens).replace(',', ' ')
-                review_score = get_review_score(review_tokens)
+                # 该评论的星级评分
+                star = linecache.getline(score_path, k)
+
+                review_score = get_review_score(review_tokens, star, k)
+                if k <= 3:
+                    print(star)
+                    print(review_score)
                 if review_score > 0:
                     p_count += 1
                 elif review_score < 0:
@@ -114,6 +164,7 @@ def get_score_file(rev_file, score_file):
                     z_count += 1
                 wfile.write(str(review_score))
                 wfile.write('\n')
+                k += 1
     print('正：', p_count)
     print('负：', n_count)
     print('零：', z_count)
@@ -127,5 +178,8 @@ ishdict = get_word_dict(ish_path)
 insufficientdict = get_word_dict(insufficient_path)
 inversedict = get_word_dict(inverse_path)
 
-get_score_file(path1, neg_score_path)
-get_score_file(path2, pos_score_path)
+print("消极")
+get_score_file(path1, neg_score_path, score_neg)
+print("============")
+print("积极")
+get_score_file(path2, pos_score_path, score_pos)
